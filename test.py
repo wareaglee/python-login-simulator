4686d1cd66646e7e44aab8bab9948c39034f2d74
wareaglee <kobialvarez90@gmail.com>
2021-06-17 10:25:24 -0400

    Made code according to PEP guidelines

--- test.py
+++ test.py
@@ -1,34 +1,36 @@
 accounts = {}
-def mAccount() :
-    Host = input("Please enter your Username : ")
-    while Host in accounts:
-        if Host in accounts:
-            pass_key = input(f"{Host} Please enter your Password : ")
+
+def my_account() :
+    host = input("Please enter your Username : ")
+    while host in accounts:
+        if host in accounts:
+            pass_key = input(f"{host} Please enter your Password : ")
     
-            if accounts[Host] == pass_key:
+            if accounts[host] == pass_key:
                 print("You've entered the secret chambers of programing")
     
             else:
-                print(f"{Host} has entered the wrong password please try again.")
+                print(f"{host} has entered the wrong password please try again.")
         else:
-            print(f"{Host} was not found please try agin or create an account.")
-            return noAccount()
-def noAccount() :
+            print(f"{host} was not found please try agin or create an account.")
+            return no_account()
+
+def no_account() :
     username = input("Enter a Username : ")
     if username in accounts:
         print(f"{username} already exist please try agian.")
-
+        
     else: 
         passwords = input("Enter a Password : ")
         accounts[username] = passwords
         print(f"Thanks for signing up {username}")
 
-User = input("Have you made an account before? please type YES/NO : ").lower() # take user input / choice and lower case it for comparison below
-if User == "yes" :
-    mAccount()
+user = input("Have you made an account before? please type YES/NO : ").lower() # take user input / choice and lower case it for comparison below
+if user == "yes" :
+    my_account()
     
 else: 
-    noAccount()
+    no_account()
     
 
 
